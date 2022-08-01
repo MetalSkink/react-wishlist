ebda79c9310e1170c22053e8f6b7f640309223d8
Alucard <xavier_ortega98@hotmail.com>
2022-08-01 15:51:12 -0500

    Products modified

--- src/assets/products.js
+++ src/assets/products.js
@@ -5,6 +5,11 @@ export const productos = [
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1445438675-dark-nights-death-metal-dc-comics-deluxe-_JM#position=4&search_layout=stack&type=item&tracking_id=b1e10a0f-87be-4682-9da0-782438f0c9e2'
   },
   {
+    nombre: 'DC Multiverse - Batman Versión 2 - Batman Movie',
+    img: 'https://m.media-amazon.com/images/I/71nhHHYmXIL._AC_SL1166_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/B09MLKRH5N/ref=ewc_pr_img_1?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  {
     nombre: 'Justice League La Totalidad',
     img: 'https://http2.mlstatic.com/D_NQ_NP_2X_799538-MLM41564787619_042020-F.webp',
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-772228533-justice-league-la-totalidad-_JM'
@@ -30,9 +35,9 @@ export const productos = [
     buyLink: 'https://www.amazon.com.mx/gp/product/1607067722/ref=ox_sc_saved_title_9?smid=AVDBXBAVVSXLQ&psc=1'
   },
   {
-    nombre: 'Batman by Scott Snyder & Greg Capullo Omnibus Vol. 1',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91WUu-gqPeL.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/1401298842/ref=ox_sc_act_title_1?smid=AVDBXBAVVSXLQ&psc=1'
+    nombre: 'Injustice Gods Among Us: Año Cinco La Coleccion Completa',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_731382-MLM43603969944_092020-F.webp',
+    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-754571653-injustice-gods-among-us-ano-cinco-la-coleccion-completa-_JM#position=3&search_layout=grid&type=item&tracking_id=7dae74de-9506-468b-8e44-69876814b6db'
   },
   {
     nombre: 'DC Multiverse - Green Lantern Modern Comic',
@@ -40,23 +45,13 @@ export const productos = [
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-962667469-figura-green-lantern-john-stewart-multiverse-linterna-verde-_JM#position=6&search_layout=stack&type=item&tracking_id=be2c0505-5492-48e7-8380-b49b05010c3f'
   },
   {
-    nombre: 'McFarlane - DC Build-A 7In Figures Wave 4 - Death Metal - Wonder Woman',
-    img: 'https://m.media-amazon.com/images/I/71gHSs12YGL._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/dp/B08FP8BZL3/?coliid=I1C3JYSL5AJS82&colid=RBQ15CHVVBLV&psc=1&ref_=lv_ov_lig_dp_it'
-  },
-  {
-    nombre: 'McFarlane Toys DC Multiverse The Dark Knight Return Batman',
-    img: 'https://m.media-amazon.com/images/I/51E-bMUwVXL._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/dp/B09BF32X81/?coliid=IVW08CS7VNBOG&colid=RBQ15CHVVBLV&psc=1&ref_=lv_ov_lig_dp_it'
-  },
-  {
-    nombre: 'McFarlane Toys Spawn Action Figures Deluxe Box Set',
-    img: 'https://m.media-amazon.com/images/I/81j2cQAwX0L._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/dp/B09F84N768/?coliid=I267Z7JZI4K9XQ&colid=RBQ15CHVVBLV&psc=1&ref_=lv_ov_lig_dp_it'
-  },
-  {
     nombre: 'Batman Damned',
     img: 'https://http2.mlstatic.com/D_NQ_NP_2X_757170-MLM41973434034_052020-F.webp',
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-779812349-batman-damned-td-_JM#position=1&search_layout=stack&type=item&tracking_id=bdbfb43d-c84f-4d9f-97af-c9dd01e8b30c'
+  },
+  {
+    nombre: 'Dragon Ball Super N.16',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71-dEkSNu4L.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/6075687548/ref=ox_sc_saved_image_1?smid=AVDBXBAVVSXLQ&psc=1'
   },
 ]
