518e9a9bb96f56851f20ce558b37aa14db1bbea8
Alucard <xavier_ortega98@hotmail.com>
2022-08-03 22:08:42 -0500

    Products modified 03/08

--- src/assets/products.js
+++ src/assets/products.js
@@ -5,24 +5,9 @@ export const productos = [
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1445438675-dark-nights-death-metal-dc-comics-deluxe-_JM#position=4&search_layout=stack&type=item&tracking_id=b1e10a0f-87be-4682-9da0-782438f0c9e2'
   },
   {
-    nombre: 'DC Multiverse - Batman Versión 2 - Batman Movie',
-    img: 'https://m.media-amazon.com/images/I/71nhHHYmXIL._AC_SL1166_.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/B09MLKRH5N/ref=ewc_pr_img_1?smid=AVDBXBAVVSXLQ&psc=1'
-  },
-  {
-    nombre: 'Justice League La Totalidad',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_799538-MLM41564787619_042020-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-772228533-justice-league-la-totalidad-_JM'
-  },
-  {
-    nombre: 'Justice League Cementerio De Dioses',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_602078-MLM41586218379_042020-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-772716584-justice-league-cementerio-de-dioses-_JM'
-  },
-  {
-    nombre: 'Liga De La Justicia Y Un Misterio En El Espacio',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_696349-MLM41973354858_052020-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-779819647-liga-de-la-justicia-y-un-misterio-en-el-espacio-scott-snyder-_JM'
+    nombre: 'Juego de tronos (Canción de Hielo y Fuego 1)',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/61royjw7ITL.jpg',
+    buyLink: 'https://www.amazon.com.mx/Juego-tronos-Canción-Hielo-Fuego/dp/6073128835/ref=sr_1_1?__mk_es_MX=ÅMÅŽÕÑ&crid=2TE6DHJNDP5LA&keywords=juego+de+tronos&qid=1659539952&sprefix=juego+de+tronos%2Caps%2C123&sr=8-1'
   },
   {
     nombre: 'Aquaman by Geoff Johns Omnibus',
@@ -40,9 +25,14 @@ export const productos = [
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-754571653-injustice-gods-among-us-ano-cinco-la-coleccion-completa-_JM#position=3&search_layout=grid&type=item&tracking_id=7dae74de-9506-468b-8e44-69876814b6db'
   },
   {
-    nombre: 'DC Multiverse - Green Lantern Modern Comic',
-    img: 'https://m.media-amazon.com/images/I/71D+nS24kUL._AC_SL1500_.jpg',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-962667469-figura-green-lantern-john-stewart-multiverse-linterna-verde-_JM#position=6&search_layout=stack&type=item&tracking_id=be2c0505-5492-48e7-8380-b49b05010c3f'
+    nombre: 'DC Multiverse - Batman Versión 2 - Batman Movie',
+    img: 'https://m.media-amazon.com/images/I/71nhHHYmXIL._AC_SL1166_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/B09MLKRH5N/ref=ewc_pr_img_1?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  {
+    nombre: 'Dragon Ball Super N.16',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71-dEkSNu4L.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/6075687548/ref=ox_sc_saved_image_1?smid=AVDBXBAVVSXLQ&psc=1'
   },
   {
     nombre: 'Batman Damned',
@@ -50,8 +40,8 @@ export const productos = [
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-779812349-batman-damned-td-_JM#position=1&search_layout=stack&type=item&tracking_id=bdbfb43d-c84f-4d9f-97af-c9dd01e8b30c'
   },
   {
-    nombre: 'Dragon Ball Super N.16',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/71-dEkSNu4L.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/6075687548/ref=ox_sc_saved_image_1?smid=AVDBXBAVVSXLQ&psc=1'
+    nombre: 'Logitech Z150 Bocina',
+    img: 'https://m.media-amazon.com/images/I/617hRfDtCsL._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Logitech-Bocina-Alámbrico-color-Negro/dp/B00IP5PTTE/ref=sr_1_6?__mk_es_MX=ÅMÅŽÕÑ&crid=17I376JQXQUAK&keywords=bocinas&qid=1659540126&sprefix=bocina%2Caps%2C122&sr=8-6'
   },
 ]
