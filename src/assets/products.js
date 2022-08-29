f7ce9cb744ddd9e3803996567585682918503799
Alucard <xavier_ortega98@hotmail.com>
2022-08-29 00:20:05 -0500

    Products modified 29/08

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,28 +1,53 @@
 export const productos = [
   {
-    nombre: 'Dark Nights Death Metal - Dc Comics Deluxe',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_887153-MLM50437229923_062022-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1445438675-dark-nights-death-metal-dc-comics-deluxe-_JM#position=4&search_layout=stack&type=item&tracking_id=b1e10a0f-87be-4682-9da0-782438f0c9e2'
+    nombre: 'The Amazing Spider-man Colección Definitiva Libro 4 - Marvel',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_607170-MLM50655206834_072022-F.webp',
+    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1455727258-the-amazing-spider-man-coleccion-definitiva-libro-4-marvel-_JM#position=7&search_layout=grid&type=item&tracking_id=1032778e-49f2-412b-ae55-3dd542d886bf'
   },
+  {
+    nombre: 'Spider- Man Blue Deluxe',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_977065-MLM43942331321_102020-F.webp',
+    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-833490710-spider-man-blue-deluxe-_JM'
+  },
+  {
+    nombre: 'The Superior Spider-man La Colección Completa Volumen 1',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_638998-MLM41499303130_042020-F.webp',
+    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-770915278-the-superior-spider-man-la-coleccion-completa-vol1-_JM'
+  },
+  {
+    nombre: 'The Superior Spider-man La Colección Completa Volumen 2',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_613235-MLM41975282612_052020-F.webp',
+    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-779855103-the-superior-spider-man-la-coleccion-completa-volumen-2-_JM'
+  },
+  {
+    nombre: 'Marvel Legends Gamerverse: Spider Man Miles Morales',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_766684-MLM48245050315_112021-F.webp',
+    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1341569646-marvel-legends-gamerverse-spider-man-miles-morales-en-stock-_JM#position=5&search_layout=stack&type=item&tracking_id=86d66205-98ab-472c-92c8-27b886ab7a07'
+  },
+  {
+    nombre: 'GUARDIÁN DE NUEVA YORK Mediana',
+    img: 'https://cdn.shopify.com/s/files/1/2997/2714/products/16093_590x.jpg?v=1657117488',
+    buyLink: 'https://mascaradelatex.com/products/playera-para-hombre-guardian-de-nueva-york'
+  },
+  {
+    nombre: 'Amazing Spider-Man by J. Michael Straczynski Omnibus Vol. 1 Hc',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71o49mdTkOL.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1302945440/ref=ox_sc_saved_image_2?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  // {
+  //   nombre: '',
+  //   img: '',
+  //   buyLink: ''
+  // },
   {
     nombre: 'Juego de tronos (Canción de Hielo y Fuego 1)',
     img: 'https://images-na.ssl-images-amazon.com/images/I/61royjw7ITL.jpg',
     buyLink: 'https://www.amazon.com.mx/Juego-tronos-Canción-Hielo-Fuego/dp/6073128835/ref=sr_1_1?__mk_es_MX=ÅMÅŽÕÑ&crid=2TE6DHJNDP5LA&keywords=juego+de+tronos&qid=1659539952&sprefix=juego+de+tronos%2Caps%2C123&sr=8-1'
   },
   {
-    nombre: 'Aquaman by Geoff Johns Omnibus',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/A1-bTmKIA5L.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/1401285465/ref=ox_sc_act_title_1?smid=AVDBXBAVVSXLQ&psc=1'
-  },
-  {
     nombre: 'Invincible Compendium Volume 2',
     img: 'https://images-na.ssl-images-amazon.com/images/I/71sGkh+82DL.jpg',
     buyLink: 'https://www.amazon.com.mx/gp/product/1607067722/ref=ox_sc_saved_title_9?smid=AVDBXBAVVSXLQ&psc=1'
-  },
-  {
-    nombre: 'Injustice Gods Among Us: Año Cinco La Coleccion Completa',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_731382-MLM43603969944_092020-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-754571653-injustice-gods-among-us-ano-cinco-la-coleccion-completa-_JM#position=3&search_layout=grid&type=item&tracking_id=7dae74de-9506-468b-8e44-69876814b6db'
   },
   {
     nombre: 'DC Multiverse - Batman Versión 2 - Batman Movie',
@@ -33,11 +58,6 @@ export const productos = [
     nombre: 'Dragon Ball Super N.16',
     img: 'https://images-na.ssl-images-amazon.com/images/I/71-dEkSNu4L.jpg',
     buyLink: 'https://www.amazon.com.mx/gp/product/6075687548/ref=ox_sc_saved_image_1?smid=AVDBXBAVVSXLQ&psc=1'
-  },
-  {
-    nombre: 'Batman Damned',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_757170-MLM41973434034_052020-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-779812349-batman-damned-td-_JM#position=1&search_layout=stack&type=item&tracking_id=bdbfb43d-c84f-4d9f-97af-c9dd01e8b30c'
   },
   {
     nombre: 'Logitech Z150 Bocina',
