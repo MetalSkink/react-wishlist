6d67e64fa33af989a05af713f6146ee87702b6f4
Alucard <xavier_ortega98@hotmail.com>
2022-09-20 00:13:10 -0500

    Products modified 20/09

--- src/assets/products.js
+++ src/assets/products.js
@@ -9,30 +9,15 @@ export const productos = [
     img: 'https://http2.mlstatic.com/D_NQ_NP_2X_977065-MLM43942331321_102020-F.webp',
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-833490710-spider-man-blue-deluxe-_JM'
   },
+  // {
+  //   nombre: 'The Superior Spider-man La Colección Completa Volumen 2',
+  //   img: 'https://http2.mlstatic.com/D_NQ_NP_2X_613235-MLM41975282612_052020-F.webp',
+  //   buyLink: 'https://articulo.mercadolibre.com.mx/MLM-779855103-the-superior-spider-man-la-coleccion-completa-volumen-2-_JM'
+  // },
   {
-    nombre: 'The Superior Spider-man La Colección Completa Volumen 1',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_638998-MLM41499303130_042020-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-770915278-the-superior-spider-man-la-coleccion-completa-vol1-_JM'
-  },
-  {
-    nombre: 'The Superior Spider-man La Colección Completa Volumen 2',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_613235-MLM41975282612_052020-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-779855103-the-superior-spider-man-la-coleccion-completa-volumen-2-_JM'
-  },
-  {
-    nombre: 'Marvel Legends Gamerverse: Spider Man Miles Morales',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_766684-MLM48245050315_112021-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1341569646-marvel-legends-gamerverse-spider-man-miles-morales-en-stock-_JM#position=5&search_layout=stack&type=item&tracking_id=86d66205-98ab-472c-92c8-27b886ab7a07'
-  },
-  {
-    nombre: 'GUARDIÁN DE NUEVA YORK Mediana',
-    img: 'https://cdn.shopify.com/s/files/1/2997/2714/products/16093_590x.jpg?v=1657117488',
-    buyLink: 'https://mascaradelatex.com/products/playera-para-hombre-guardian-de-nueva-york'
-  },
-  {
-    nombre: 'Amazing Spider-Man by J. Michael Straczynski Omnibus Vol. 1 Hc',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/71o49mdTkOL.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/1302945440/ref=ox_sc_saved_image_2?smid=AVDBXBAVVSXLQ&psc=1'
+    nombre: 'Aquaman by Geoff Johns Omnibus',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/A1-bTmKIA5L.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1401285465/ref=ox_sc_saved_image_2?smid=AVDBXBAVVSXLQ&psc=1'
   },
   // {
   //   nombre: '',
@@ -40,24 +25,39 @@ export const productos = [
   //   buyLink: ''
   // },
   {
-    nombre: 'Juego de tronos (Canción de Hielo y Fuego 1)',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/61royjw7ITL.jpg',
-    buyLink: 'https://www.amazon.com.mx/Juego-tronos-Canción-Hielo-Fuego/dp/6073128835/ref=sr_1_1?__mk_es_MX=ÅMÅŽÕÑ&crid=2TE6DHJNDP5LA&keywords=juego+de+tronos&qid=1659539952&sprefix=juego+de+tronos%2Caps%2C123&sr=8-1'
-  },
-  {
     nombre: 'Invincible Compendium Volume 2',
     img: 'https://images-na.ssl-images-amazon.com/images/I/71sGkh+82DL.jpg',
     buyLink: 'https://www.amazon.com.mx/gp/product/1607067722/ref=ox_sc_saved_title_9?smid=AVDBXBAVVSXLQ&psc=1'
   },
   {
-    nombre: 'DC Multiverse - Batman Versión 2 - Batman Movie',
-    img: 'https://m.media-amazon.com/images/I/71nhHHYmXIL._AC_SL1166_.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/B09MLKRH5N/ref=ewc_pr_img_1?smid=AVDBXBAVVSXLQ&psc=1'
+    nombre: 'Marvel Legends Series - Hobgoblin',
+    img: 'https://m.media-amazon.com/images/I/81ZmFDUJAXL._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/B09DQ9XBDS/ref=ox_sc_saved_image_6?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  {
+    nombre: 'Marvel Legends Series - Shocker',
+    img: 'https://m.media-amazon.com/images/I/81jvV7mgx7L._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/B09DQ7YB4X/ref=ox_sc_saved_image_7?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  {
+    nombre: 'Call of duty Modern Warfare 2',
+    img: 'https://imageio.forbes.com/specials-images/imageserve/628d337e791f9767c05ca2e7/1--2-/960x0.jpg?height=887&width=711&fit=bounds',
+    buyLink: 'https://store.steampowered.com/app/1938090/Call_of_Duty_Modern_Warfare_II/'
+  },
+  {
+    nombre: 'Batman by Scott Snyder & Greg Capullo Omnibus',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/91WUu-gqPeL.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1401298842/ref=ox_sc_saved_image_4?smid=AVDBXBAVVSXLQ&psc=1'
   },
   {
     nombre: 'Dragon Ball Super N.16',
     img: 'https://images-na.ssl-images-amazon.com/images/I/71-dEkSNu4L.jpg',
     buyLink: 'https://www.amazon.com.mx/gp/product/6075687548/ref=ox_sc_saved_image_1?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  {
+    nombre: 'GUARDIÁN DE NUEVA YORK Mediana',
+    img: 'https://cdn.shopify.com/s/files/1/2997/2714/products/16093_590x.jpg?v=1657117488',
+    buyLink: 'https://mascaradelatex.com/products/playera-para-hombre-guardian-de-nueva-york'
   },
   {
     nombre: 'Logitech Z150 Bocina',
