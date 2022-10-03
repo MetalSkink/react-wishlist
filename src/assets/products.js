3fe5726a1de48afa7f8bbacd0695cc913f8fc735
Alucard <xavier_ortega98@hotmail.com>
2022-10-03 12:49:15 -0500

    Products modified 03/10

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,19 +1,24 @@
 export const productos = [
   {
+    nombre: 'The last Ronin',
+    img: 'https://m.media-amazon.com/images/I/519isozjeCL._SX330_BO1,204,203,200_.jpg',
+    buyLink: 'https://www.amazon.com.mx/dp/1684058414/ref=cm_sw_r_as_gl_api_gl_i_4RC9D92ADHJ5488RPTVJ?_encoding=UTF8&psc=1&linkCode=ml1&tag=ofertasgeek-20'
+  },
+  {
     nombre: 'The Amazing Spider-man Colección Definitiva Libro 4 - Marvel',
     img: 'https://http2.mlstatic.com/D_NQ_NP_2X_607170-MLM50655206834_072022-F.webp',
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1455727258-the-amazing-spider-man-coleccion-definitiva-libro-4-marvel-_JM#position=7&search_layout=grid&type=item&tracking_id=1032778e-49f2-412b-ae55-3dd542d886bf'
-  },
-  {
-    nombre: 'Spider- Man Blue Deluxe',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_977065-MLM43942331321_102020-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-833490710-spider-man-blue-deluxe-_JM'
   },
   // {
   //   nombre: 'The Superior Spider-man La Colección Completa Volumen 2',
   //   img: 'https://http2.mlstatic.com/D_NQ_NP_2X_613235-MLM41975282612_052020-F.webp',
   //   buyLink: 'https://articulo.mercadolibre.com.mx/MLM-779855103-the-superior-spider-man-la-coleccion-completa-volumen-2-_JM'
   // },
+  {
+    nombre: 'Daredevil by Charles Soule Omnibus',
+    img: 'https://m.media-amazon.com/images/I/61HwrxJKmxL.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1302929658/ref=ox_sc_saved_title_1?smid=AVDBXBAVVSXLQ&psc=1'
+  },
   {
     nombre: 'Aquaman by Geoff Johns Omnibus',
     img: 'https://images-na.ssl-images-amazon.com/images/I/A1-bTmKIA5L.jpg',
@@ -39,15 +44,25 @@ export const productos = [
     img: 'https://m.media-amazon.com/images/I/81jvV7mgx7L._AC_SL1500_.jpg',
     buyLink: 'https://www.amazon.com.mx/gp/product/B09DQ7YB4X/ref=ox_sc_saved_image_7?smid=AVDBXBAVVSXLQ&psc=1'
   },
+  // {
+  //   nombre: '',
+  //   img: '',
+  //   buyLink: ''
+  // },
   {
-    nombre: 'Call of duty Modern Warfare 2',
-    img: 'https://imageio.forbes.com/specials-images/imageserve/628d337e791f9767c05ca2e7/1--2-/960x0.jpg?height=887&width=711&fit=bounds',
-    buyLink: 'https://store.steampowered.com/app/1938090/Call_of_Duty_Modern_Warfare_II/'
+    nombre: 'Metroid Dread',
+    img: 'https://m.media-amazon.com/images/I/7167nQX+-4S._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Metroid-Dread-Standard-Nintendo-Switch/dp/B098SL8LY9/ref=sr_1_16?keywords=juegos+nintendo+switch&qid=1664818317&qu=eyJxc2MiOiI4LjE4IiwicXNhIjoiNy4yMiIsInFzcCI6IjYuMDAifQ%3D%3D&sprefix=juegos+%2Caps%2C131&sr=8-16'
   },
   {
-    nombre: 'Batman by Scott Snyder & Greg Capullo Omnibus',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/91WUu-gqPeL.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/1401298842/ref=ox_sc_saved_image_4?smid=AVDBXBAVVSXLQ&psc=1'
+    nombre: 'The Legend of Zelda: Skyward Sword HD',
+    img: 'https://m.media-amazon.com/images/I/81ty2Tz3OfS._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Legend-Zelda-Skyward-Standard-Nintendo/dp/B092YJ62Y6/ref=sr_1_32?keywords=juegos+nintendo+switch&qid=1664818436&qu=eyJxc2MiOiI4LjE4IiwicXNhIjoiNy4yMiIsInFzcCI6IjYuMDAifQ%3D%3D&sprefix=juegos+%2Caps%2C131&sr=8-32'
+  },
+  {
+    nombre: 'New Super Mario Bros. U Deluxe',
+    img: 'https://m.media-amazon.com/images/I/71Sv4dPnq7S._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/New-Super-Mario-Bros-Deluxe/dp/B09361WPHW/ref=sr_1_17?keywords=juegos+nintendo+switch&qid=1664818436&qu=eyJxc2MiOiI4LjE4IiwicXNhIjoiNy4yMiIsInFzcCI6IjYuMDAifQ%3D%3D&sprefix=juegos+%2Caps%2C131&sr=8-17'
   },
   {
     nombre: 'Dragon Ball Super N.16',
