d3a75e4bebc5e3475d16dc7cbfbb24747d41c9c3
Alucard <xavier_ortega98@hotmail.com>
2022-10-19 17:49:42 -0500

    Products modified 19/10

--- src/assets/products.js
+++ src/assets/products.js
@@ -5,24 +5,14 @@ export const productos = [
     buyLink: 'https://www.amazon.com.mx/dp/1684058414/ref=cm_sw_r_as_gl_api_gl_i_4RC9D92ADHJ5488RPTVJ?_encoding=UTF8&psc=1&linkCode=ml1&tag=ofertasgeek-20'
   },
   {
-    nombre: 'Daredevil Yellow Deluxe',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_685983-MLM44063280125_112020-F.webp',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-838716178-daredevil-yellow-deluxe-_JM#reco_item_pos=1&reco_backend=machinalis-homes-pdp-boos&reco_backend_type=function&reco_client=home_navigation-recommendations&reco_id=7044c20d-0577-4c8c-ad12-1a64dbdf7e2f&c_id=/home/navigation-recommendations/element&c_element_order=2&c_uid=cb88ce7d-d6d8-49dc-a868-c31248c70c49'
+    nombre: 'La Edad De Bronce Swamp Thing Clásico',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_958385-MLM45413147068_042021-F.webp',
+    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-769188264-la-edad-de-bronce-swamp-thing-clasico-_JM'
   },
   {
     nombre: 'The Amazing Spider-man Colección Definitiva Libro 4 - Marvel',
     img: 'https://http2.mlstatic.com/D_NQ_NP_2X_607170-MLM50655206834_072022-F.webp',
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1455727258-the-amazing-spider-man-coleccion-definitiva-libro-4-marvel-_JM#position=7&search_layout=grid&type=item&tracking_id=1032778e-49f2-412b-ae55-3dd542d886bf'
-  },
-  // {
-  //   nombre: 'The Superior Spider-man La Colección Completa Volumen 2',
-  //   img: 'https://http2.mlstatic.com/D_NQ_NP_2X_613235-MLM41975282612_052020-F.webp',
-  //   buyLink: 'https://articulo.mercadolibre.com.mx/MLM-779855103-the-superior-spider-man-la-coleccion-completa-volumen-2-_JM'
-  // },
-  {
-    nombre: 'Daredevil by Charles Soule Omnibus',
-    img: 'https://m.media-amazon.com/images/I/61HwrxJKmxL.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/1302929658/ref=ox_sc_saved_title_1?smid=AVDBXBAVVSXLQ&psc=1'
   },
   {
     nombre: 'Aquaman by Geoff Johns Omnibus',
@@ -30,14 +20,24 @@ export const productos = [
     buyLink: 'https://www.amazon.com.mx/gp/product/1401285465/ref=ox_sc_saved_image_2?smid=AVDBXBAVVSXLQ&psc=1'
   },
   // {
-  //   nombre: '',
-  //   img: '',
-  //   buyLink: ''
-  // },
-  {
-    nombre: 'Invincible Compendium Volume 2',
+    //   nombre: '',
+    //   img: '',
+    //   buyLink: ''
+    // },
+    {
+      nombre: 'Invincible Compendium Volume 2',
     img: 'https://images-na.ssl-images-amazon.com/images/I/71sGkh+82DL.jpg',
     buyLink: 'https://www.amazon.com.mx/gp/product/1607067722/ref=ox_sc_saved_title_9?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  {
+    nombre: 'STAR WARS La colección Vintage - Juguete ARC Trooper',
+    img: 'https://m.media-amazon.com/images/I/81txVzCPA-L._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/B09LVWTHDH/ref=ox_sc_saved_image_8?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  {
+    nombre: 'Mcfarlane Toys Munitions Pack Weapon',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_610868-MLM50772643740_072022-F.webp',
+    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1461860222-mcfarlane-toys-munitions-pack-weapon-exclusivo-7-pulgadas-_JM#position=10&search_layout=grid&type=item&tracking_id=f75a37da-9f92-4114-a280-744fc0c8eaa3'
   },
   {
     nombre: 'Marvel Legends Series - Hobgoblin',
