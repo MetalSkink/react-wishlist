fd43b69f2814fb5b1efefe69459f8ef4453901fc
Alucard <xavier_ortega98@hotmail.com>
2023-05-22 00:01:28 -0600

    Products modified 22/05

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,5 +1,29 @@
 export const productos = [
   {
+    nombre: 'Batman by Grant Morrison Vol 2',
+    img: 'https://m.media-amazon.com/images/I/91qib8BYH9L.jpg',
+    buyLink: 'https://www.amazon.com.mx/Batman-Grant-Morrison-Omnibus-Vol/dp/1401288839/ref=sr_1_15?keywords=batman+omnibus&qid=1684732583&s=books&sprefix=batman+om%2Cstripbooks%2C125&sr=1-15',
+    note: "Este esta a muy buen precio para ser un omnibus"
+  },
+  {
+    nombre: 'Flashpoint: The 10th Anniversary Omnibus',
+    img: 'https://m.media-amazon.com/images/I/A1BXZtRFkxS.jpg',
+    buyLink: 'https://www.amazon.com.mx/Flashpoint-Anniversary-Omnibus-Geoff-Johns/dp/1779509774/ref=pd_bxgy_img_sccl_1/137-9444642-6765259?pd_rd_w=Ff8E9&content-id=amzn1.sym.073f9082-4467-4f70-939c-f1e02d32ade1&pf_rd_p=073f9082-4467-4f70-939c-f1e02d32ade1&pf_rd_r=2PG7EFBAEYY87PSVMKC6&pd_rd_wg=UVCDW&pd_rd_r=c907c1b3-cc2f-4cba-8f5d-28ab71465310&pd_rd_i=1779509774&psc=1',
+    note: "Este si esta carito cuando lo agregue estaba en $1500 pero trae mas de 1500 paginas"
+  },
+  {
+    nombre: 'Batman by Paul Dini Omnibus',
+    img: 'https://m.media-amazon.com/images/I/91-KsoNZnUL.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1779505515/ref=ox_sc_saved_image_4?smid=AVDBXBAVVSXLQ&psc=1',
+    note: "Este esta muy caro pero lo meti por si algun dia baja de precio"
+  },
+  {
+    nombre: 'Batman by Grant Morrison Vol 3',
+    img: 'https://m.media-amazon.com/images/I/91B34-ab2eL.jpg',
+    buyLink: 'https://www.amazon.com.mx/Batman-Grant-Morrison-Omnibus-Vol/dp/1779502710/ref=sr_1_20?keywords=batman+omnibus&qid=1684732590&s=books&sprefix=batman+om%2Cstripbooks%2C125&sr=1-20',
+    note: ""
+  },
+  {
     nombre: 'Dare Devil Visionarios Vol. 2',
     img: 'https://m.media-amazon.com/images/I/81Ntt1+LKmL.jpg',
     buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1499227337-daredevil-visionarios-frank-miller-volumen-2-marvel-deluxe-_JM#position=4&search_layout=grid&type=item&tracking_id=f6c076db-cee5-4758-bf8b-be1cd4093855',
@@ -12,16 +36,10 @@ export const productos = [
     note: ""
   },
   {
-    nombre: 'The Legend of Zelda: Breath of the Wild ',
-    img: 'https://m.media-amazon.com/images/I/716IChJgUzS._AC_SL1200_.jpg',
-    buyLink: 'https://www.amazon.com.mx/dp/B086RCVVN6/?coliid=IYTTT2CB9ZIG6&colid=3CW9Y6HRWAB2Z&psc=1&ref_=list_c_wl_lv_ov_lig_dp_it',
-    note: 'Todos los jeugos de switch cambian a diario de precio no conviene comprarlos si cuestan mas de 1100'
-  },
-  {
     nombre: 'The Legend of Zelda: Skyward Sword HD ',
     img: 'https://m.media-amazon.com/images/I/81ty2Tz3OfS._AC_SL1500_.jpg',
     buyLink: 'https://www.amazon.com.mx/dp/B092YJ62Y6/?coliid=I3U0UXT5BVEDNG&colid=3CW9Y6HRWAB2Z&psc=1&ref_=lv_ov_lig_dp_it',
-    note: ''
+    note: 'Todos los juegos de switch cambian a diario de precio no conviene comprarlos si cuestan mas de 1100'
   },
   {
     nombre: 'Metroid Dread',
@@ -40,11 +58,11 @@ export const productos = [
       img: 'https://cdn.gameplanet.com/wp-content/uploads/2023/02/07195651/810050910989-estuche-hori-rigido-oled-1.jpg',
       buyLink: 'https://gameplanet.com/producto/estuche-rigido-nintendo-switch-oled-nsw/'
   },
-  {
-      nombre: 'Gomitas Texturizadas Grips Compatible Con Nintendo Switch (8 Unidades)',
-      img: 'https://m.media-amazon.com/images/I/41iE-U7HfnL._AC_SL1000_.jpg',
-      buyLink: 'https://www.amazon.com.mx/Nintendo-Gomitas-Texturizadas-Compatible-Unidades/dp/B073T7W9PY/ref=zg_bs_17715938011_sccl_4/137-9444642-6765259?psc=1'
-  },
+  // {
+  //     nombre: 'Gomitas Texturizadas Grips Compatible Con Nintendo Switch (8 Unidades)',
+  //     img: 'https://m.media-amazon.com/images/I/41iE-U7HfnL._AC_SL1000_.jpg',
+  //     buyLink: 'https://www.amazon.com.mx/Nintendo-Gomitas-Texturizadas-Compatible-Unidades/dp/B073T7W9PY/ref=zg_bs_17715938011_sccl_4/137-9444642-6765259?psc=1'
+  // },
   {
     nombre: 'HELLS PARADISE N.11',
     img: 'https://tiendapanini.com.mx/media/catalog/product/q/h/qhepa011_1200.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=897&width=960&canvas=960:897&dpr=1%201x',
@@ -62,12 +80,6 @@ export const productos = [
     img: 'https://tiendapanini.com.mx/media/catalog/product/q/h/qhepa013_1200.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=897&width=960&canvas=960:897&dpr=1%201x',
     buyLink: 'https://tiendapanini.com.mx/hells-paradise-n13',
     note: ""
-  },
-  {
-    nombre: 'Batman by Paul Dini Omnibus',
-    img: 'https://m.media-amazon.com/images/I/91-KsoNZnUL.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/1779505515/ref=ox_sc_saved_image_4?smid=AVDBXBAVVSXLQ&psc=1',
-    note: "Este esta muy caro pero lo meti por si algun dia baja de precio"
   },
   // {
   //     nombre: '',
