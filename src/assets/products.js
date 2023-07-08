420f763fa24b53748032581b0cb709c4cdde1073
Alucard <xavier_ortega98@hotmail.com>
2023-07-07 22:38:53 -0600

    Products modified 07/07

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,16 +1,34 @@
 export const productos = [
-  // {
-  //   nombre: 'Gotham Central Omnibus',
-  //   img: 'https://m.media-amazon.com/images/I/91FWAFzkIlL.jpg',
-  //   buyLink: 'https://www.amazon.com.mx/gp/product/1779515634/ref=ox_sc_saved_image_2?smid=AVDBXBAVVSXLQ&psc=1',
-  //   note: ""
-  // },
-  // {
-  //   nombre: 'Batman by Paul Dini Omnibus',
-  //   img: 'https://m.media-amazon.com/images/I/91-KsoNZnUL.jpg',
-  //   buyLink: 'https://www.amazon.com.mx/gp/product/1779505515/ref=ox_sc_saved_image_4?smid=AVDBXBAVVSXLQ&psc=1',
-  //   note: "Este esta muy caro pero lo meti por si algun dia baja de precio"
-  // },
+  {
+    nombre: 'NEW X-MEN OMNIBUS',
+    img: 'https://m.media-amazon.com/images/I/91TljmVHfuL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1302949845/ref=ox_sc_saved_image_1?smid=A1G99GVHAT2WD8&psc=1',
+    note: "Todos los omnis estan bien caros pero espero que en el prime day bajen a menos de 1500"
+  },
+  {
+    nombre: 'Final Crisis Omnibus',
+    img: 'https://m.media-amazon.com/images/I/81OCoXq-pML._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1779501404/ref=ox_sc_saved_image_10?smid=AVDBXBAVVSXLQ&psc=1',
+    note: ""
+  },
+  {
+    nombre: 'Gotham Central Omnibus',
+    img: 'https://m.media-amazon.com/images/I/91FWAFzkIlL.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1779515634/ref=ox_sc_saved_image_2?smid=AVDBXBAVVSXLQ&psc=1',
+    note: ""
+  },
+  {
+    nombre: 'JLA: The Tower of Babel',
+    img: 'https://m.media-amazon.com/images/I/91WQUDdSckL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1779509510?smid=AVDBXBAVVSXLQ&psc=1',
+    note: "Este no es omni, es un pasta dura gringo y ya bajo otros 100 pesitos"
+  },
+  {
+    nombre: 'Batman One Dark Knight',
+    img: 'https://m.media-amazon.com/images/I/81Jh-vB2TUL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Batman-One-Dark-Knight-Jock/dp/1779510284/ref=d_pd_sbs_sccl_2_2/137-9444642-6765259?pd_rd_w=bosYo&content-id=amzn1.sym.bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_p=bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_r=J9GPAV93F1SB1SVPAAH7&pd_rd_wg=wLpzz&pd_rd_r=2495d038-e053-4441-9fc4-07bd5bdcbed6&pd_rd_i=1779510284&psc=1',
+    note: "Este se ve muy bueno es igual un pasta dura gringo"
+  },
   {
     nombre: 'McFarlane Toys DC Multiverse The Dark Knight Return Batman',
     img: 'https://m.media-amazon.com/images/I/61nhlBozr2L._AC_SL1500_.jpg',
@@ -23,12 +41,12 @@ export const productos = [
     buyLink: 'https://www.mercadolibre.com.mx/marvel-deluxe-daredevil-visionarios-frank-miller-volumen-3/p/MLM21385502?pdp_filters=seller_id:403517033#searchVariation=MLM21385502&position=2&search_layout=grid&type=product&tracking_id=f6c076db-cee5-4758-bf8b-be1cd4093855',
     note: ""
   },
-  {
-    nombre: 'The Legend of Zelda: Skyward Sword HD ',
-    img: 'https://m.media-amazon.com/images/I/81ty2Tz3OfS._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/dp/B092YJ62Y6/?coliid=I3U0UXT5BVEDNG&colid=3CW9Y6HRWAB2Z&psc=1&ref_=lv_ov_lig_dp_it',
-    note: 'Todos los juegos de switch cambian a diario de precio no conviene comprarlos si cuestan mas de 1100'
-  },
+  // {
+  //   nombre: 'The Legend of Zelda: Skyward Sword HD ',
+  //   img: 'https://m.media-amazon.com/images/I/81ty2Tz3OfS._AC_SL1500_.jpg',
+  //   buyLink: 'https://www.amazon.com.mx/dp/B092YJ62Y6/?coliid=I3U0UXT5BVEDNG&colid=3CW9Y6HRWAB2Z&psc=1&ref_=lv_ov_lig_dp_it',
+  //   note: 'Todos los juegos de switch cambian a diario de precio no conviene comprarlos si cuestan mas de 1100'
+  // },
   {
     nombre: 'Metroid Dread',
     img: 'https://http2.mlstatic.com/D_NQ_NP_619582-MLA47874649958_102021-O.webp',
@@ -46,48 +64,15 @@ export const productos = [
       img: 'https://cdn.gameplanet.com/wp-content/uploads/2023/02/07195651/810050910989-estuche-hori-rigido-oled-1.jpg',
       buyLink: 'https://gameplanet.com/producto/estuche-rigido-nintendo-switch-oled-nsw/'
   },
-  // {
-  //     nombre: 'Gomitas Texturizadas Grips Compatible Con Nintendo Switch (8 Unidades)',
-  //     img: 'https://m.media-amazon.com/images/I/41iE-U7HfnL._AC_SL1000_.jpg',
-  //     buyLink: 'https://www.amazon.com.mx/Nintendo-Gomitas-Texturizadas-Compatible-Unidades/dp/B073T7W9PY/ref=zg_bs_17715938011_sccl_4/137-9444642-6765259?psc=1'
-  // },
-  {
-      nombre: 'EL INMORTAL HULK VOL.03',
-      img: 'https://d14d9vp3wdof84.cloudfront.net/image/589816272436/image_of2ivd1dhl4a1akp9khq09225r/-S897-FWEBP',
-      buyLink: 'https://tiendapanini.com.mx/el-inmortal-hulk-vol03-de-12'
-  },
-  {
-      nombre: 'EL INMORTAL HULK VOL.04',
-      img: 'https://d14d9vp3wdof84.cloudfront.net/image/589816272436/image_tjntcoch2h4598657n05c9cj7h/-S897-FWEBP',
-      buyLink: 'https://tiendapanini.com.mx/el-inmortal-hulk-vol04-de-12'
-  },
-  {
-    nombre: 'CHAINSAW MAN N.12',
-    img: 'https://d14d9vp3wdof84.cloudfront.net/image/589816272436/image_871qje5sph31pemon5s3cfu75a/-S897-FWEBP'
-  },
-  {
-    nombre: 'HELLS PARADISE N.11',
-    img: 'https://tiendapanini.com.mx/media/catalog/product/q/h/qhepa011_1200.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=897&width=960&canvas=960:897&dpr=1%201x',
-    buyLink: 'https://tiendapanini.com.mx/hells-paradise-n11',
-    note: "ya nomas me faltan estos 3 para acabar la serie"
-  },
-  {
-    nombre: 'HELLS PARADISE N.12',
-    img: 'https://tiendapanini.com.mx/media/catalog/product/q/h/qhepa012_1200.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=897&width=960&canvas=960:897&dpr=1%201x',
-    buyLink: 'https://tiendapanini.com.mx/hells-paradise-n12',
-    note: ""
-  },
-  {
-    nombre: 'HELLS PARADISE N.13',
-    img: 'https://tiendapanini.com.mx/media/catalog/product/q/h/qhepa013_1200.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=897&width=960&canvas=960:897&dpr=1%201x',
-    buyLink: 'https://tiendapanini.com.mx/hells-paradise-n13',
-    note: ""
-  },
   {
     nombre: 'Invincible Compendium Volume 2',
     img: 'https://images-na.ssl-images-amazon.com/images/I/71sGkh+82DL.jpg',
     buyLink: 'https://www.amazon.com.mx/gp/product/1607067722/ref=ox_sc_saved_title_9?smid=AVDBXBAVVSXLQ&psc=1',
-    note: "Este justo ahorita esta en su mas bajo historico"
+  },
+  {
+    nombre: 'CHAINSAW MAN N.12',
+    img: 'https://d14d9vp3wdof84.cloudfront.net/image/589816272436/image_871qje5sph31pemon5s3cfu75a/-S897-FWEBP',
+    note: "Este lo encuentras en sanborns y todo julio tiene descuento"
   },
   {
     nombre: 'STAR WARS La colección Vintage - Juguete ARC Trooper',
