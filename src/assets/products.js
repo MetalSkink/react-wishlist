6eee1a4560524a439b1e5efaf83d8e5da0806b90
Xavier <xavier_ortega98@hotmail.com>
2023-09-25 18:28:04 -0600

    Products modified 25/09

--- src/assets/products.js
+++ src/assets/products.js
@@ -11,6 +11,16 @@ export const productos = [
   //   note: ""
   // },
   {
+    nombre: 'McFarlane Megafigura de Accion DC Multiverse Clayface',
+    img: 'https://m.media-amazon.com/images/I/71+Q-EHk0dL._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/dp/B09DHGWLGK/?coliid=IBYME0W4XU3F8&colid=13QCSLA5P0HV2&psc=1&ref_=list_c_wl_lv_ov_lig_dp_it'
+  },
+  {
+    nombre: 'Scarecrow mcfarlane',
+    img: 'https://m.media-amazon.com/images/I/714xZ5te5gL._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/McFarlane-Gaming-Figures-Wave-Scarecrow/dp/B09V6PJYGC/ref=sr_1_6?__mk_es_MX=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3OB4QPGFUDX07&keywords=batman+scarecrow+mcfarlane&qid=1695687055&sprefix=batman+scarecrow+mcfarlane%2Caps%2C104&sr=8-6&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47'
+  },
+  {
     nombre: 'Batman: Detective Comics 1000: The Deluxe Edition',
     img: 'https://m.media-amazon.com/images/I/91GdEjgjPwL.jpg',
     buyLink: 'https://www.amazon.com.mx/Detective-Comics-1000-Deluxe-Various/dp/1401294197/ref=sr_1_1?keywords=detective+comics+1000&qid=1692732668&sprefix=detective+comics%2Caps%2C114&sr=8-1&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47'
@@ -27,23 +37,23 @@ export const productos = [
     buyLink: 'https://www.amazon.com.mx/gp/product/1779515634/ref=ox_sc_saved_image_2?smid=AVDBXBAVVSXLQ&psc=1',
     note: "Este anda a buen precio"
   },
-  {
-    nombre: 'Teenage Mutant Ninja Turtles: The Last Ronin ',
-    img: 'https://m.media-amazon.com/images/I/81kQlzqKP-L.jpg',
-    buyLink: 'https://www.amazon.com.mx/Teenage-Mutant-Ninja-Turtles-Ronin/dp/1684058414/ref=sr_1_1?__mk_es_MX=ÅMÅŽÕÑ&crid=2QGQFD61W4089&keywords=The+Last+Ronin&qid=1692425842&sprefix=the+last+ronin%2Caps%2C105&sr=8-1&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47',
-  },
-  {
-    nombre: 'JLA: The Tower of Babel',
-    img: 'https://m.media-amazon.com/images/I/91WQUDdSckL._SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/1779509510?smid=AVDBXBAVVSXLQ&psc=1',
-    note: "Este no es omni, es un pasta dura gringo "
-  },
-  {
-    nombre: 'Batman One Dark Knight',
-    img: 'https://m.media-amazon.com/images/I/81Jh-vB2TUL._SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/Batman-One-Dark-Knight-Jock/dp/1779510284/ref=d_pd_sbs_sccl_2_2/137-9444642-6765259?pd_rd_w=bosYo&content-id=amzn1.sym.bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_p=bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_r=J9GPAV93F1SB1SVPAAH7&pd_rd_wg=wLpzz&pd_rd_r=2495d038-e053-4441-9fc4-07bd5bdcbed6&pd_rd_i=1779510284&psc=1',
-    note: "Este se ve muy bueno es igual un pasta dura gringo"
-  },
+  // {
+  //   nombre: 'Teenage Mutant Ninja Turtles: The Last Ronin ',
+  //   img: 'https://m.media-amazon.com/images/I/81kQlzqKP-L.jpg',
+  //   buyLink: 'https://www.amazon.com.mx/Teenage-Mutant-Ninja-Turtles-Ronin/dp/1684058414/ref=sr_1_1?__mk_es_MX=ÅMÅŽÕÑ&crid=2QGQFD61W4089&keywords=The+Last+Ronin&qid=1692425842&sprefix=the+last+ronin%2Caps%2C105&sr=8-1&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47',
+  // },
+  // {
+  //   nombre: 'JLA: The Tower of Babel',
+  //   img: 'https://m.media-amazon.com/images/I/91WQUDdSckL._SL1500_.jpg',
+  //   buyLink: 'https://www.amazon.com.mx/gp/product/1779509510?smid=AVDBXBAVVSXLQ&psc=1',
+  //   note: "Este no es omni, es un pasta dura gringo "
+  // },
+  // {
+  //   nombre: 'Batman One Dark Knight',
+  //   img: 'https://m.media-amazon.com/images/I/81Jh-vB2TUL._SL1500_.jpg',
+  //   buyLink: 'https://www.amazon.com.mx/Batman-One-Dark-Knight-Jock/dp/1779510284/ref=d_pd_sbs_sccl_2_2/137-9444642-6765259?pd_rd_w=bosYo&content-id=amzn1.sym.bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_p=bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_r=J9GPAV93F1SB1SVPAAH7&pd_rd_wg=wLpzz&pd_rd_r=2495d038-e053-4441-9fc4-07bd5bdcbed6&pd_rd_i=1779510284&psc=1',
+  //   note: "Este se ve muy bueno es igual un pasta dura gringo"
+  // },
   {
     nombre: 'EL INMORTAL HULK VOL.06 (DE 12)',
     img: 'https://m.media-amazon.com/images/I/71q+Eg2H9xL.jpg',
