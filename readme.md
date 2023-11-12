24f5752b87f9a5f8c030dc12be455fb424d82ade
Alucard <xavier_ortega98@hotmail.com>
2023-11-12 13:54:08 -0600

    Products modified 12/11

---
+++ readme.md
@@ -0,0 +1,20 @@
+# Editor Markdown
+
+Este es una app hecha con react que sirve para hacer tu wishlist y compartirla con quien quieras.
+## Cómo usar la app
+
+Para utilizar esta app debes de colocar en el archivo deploy.sh la ruta que tendra tu app en gh-pages.
+
+* GitHub
+* Dropbox
+* Bitbucket
+
+## Cómo subir cambios
+Para subir tus cambios debes de modificar el archivo products.js que se encuentra dentro de la carpeta assets reemplazando las propiedades de los elementos.
+
+Despues de que hagas tus cambios debes de ejecutar estos comandos
+
+* git add .
+* git commit -m "Products modified DD/MM" (reemplazando por el dia y mes correspondientes"
+* git push
+* npm run deploy
--- src/assets/products.js
+++ src/assets/products.js
@@ -1,24 +1,29 @@
 export const productos = [
-  // {
-  //   nombre: 'Nintendo Switch Pro Controller the legend of zelda tears of the kingdom edition',
-  //   img: 'https://http2.mlstatic.com/D_NQ_NP_849166-MLA69565737737_052023-O.webp',
-  //   buyLink: 'https://www.mercadolibre.com.mx/control-joystick-inalambrico-nintendo-switch-pro-controller-the-legend-of-zelda-tears-of-the-kingdom-edition/p/MLM23407222#reco_item_pos=0&reco_backend=machinalis-homes-pdp-boos&reco_backend_type=function&reco_client=home_navigation-recommendations&reco_id=08aed3db-02ce-42b9-9d88-049a73786217',
-  // },
-  // {
-  //   nombre: 'NEW X-MEN OMNIBUS',
-  //   img: 'https://m.media-amazon.com/images/I/91TljmVHfuL._SL1500_.jpg',
-  //   buyLink: 'https://www.amazon.com.mx/gp/product/1302949845/ref=ox_sc_saved_image_1?smid=A1G99GVHAT2WD8&psc=1',
-  //   note: ""
-  // },
   {
-    nombre: 'McFarlane Megafigura de Accion DC Multiverse Clayface',
-    img: 'https://m.media-amazon.com/images/I/71+Q-EHk0dL._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/dp/B09DHGWLGK/?coliid=IBYME0W4XU3F8&colid=13QCSLA5P0HV2&psc=1&ref_=list_c_wl_lv_ov_lig_dp_it'
+    nombre: 'The Animal Man Omnibus',
+    img: 'https://m.media-amazon.com/images/I/91J8WYbB0OL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1779516320/ref=ox_sc_saved_image_5?smid=AVDBXBAVVSXLQ&psc=1'
   },
   {
-    nombre: 'Scarecrow mcfarlane',
-    img: 'https://m.media-amazon.com/images/I/714xZ5te5gL._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/McFarlane-Gaming-Figures-Wave-Scarecrow/dp/B09V6PJYGC/ref=sr_1_6?__mk_es_MX=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3OB4QPGFUDX07&keywords=batman+scarecrow+mcfarlane&qid=1695687055&sprefix=batman+scarecrow+mcfarlane%2Caps%2C104&sr=8-6&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47'
+    nombre: 'Gotham Central Omnibus',
+    img: 'https://m.media-amazon.com/images/I/91FWAFzkIlL.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1779515634/ref=ox_sc_saved_image_2?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  {
+    nombre: 'Absolute Batman: The Court of Owls 2023 Edition',
+    img: 'https://m.media-amazon.com/images/I/71oLuSmb6gL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Absolute-Batman-Court-Owls-2023/dp/1779523319/ref=sr_1_2?__mk_es_MX=ÅMÅŽÕÑ&crid=2CJ2007GTNW0X&keywords=absolute+dc&qid=1699816653&sprefix=absolute+dc%2Caps%2C109&sr=8-2&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47',
+    note: 'Este acaba de salir y anda bien caro pero espero que para navidad ya baje a 1500 minimo'
+  },
+  {
+    nombre: 'Absolute Batman Year One',
+    img: 'https://m.media-amazon.com/images/I/71rrMoha4gL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1401243797/ref=ox_sc_saved_image_2?smid=AVDBXBAVVSXLQ&psc=1',
+  },
+  {
+    nombre: 'Absolute Swamp Thing by Alan Moore Vol. 2',
+    img: 'https://m.media-amazon.com/images/I/914E19HGW4L._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Absolute-Swamp-Thing-Alan-Moore/dp/1779502826/ref=sr_1_4?__mk_es_MX=ÅMÅŽÕÑ&crid=3IT74646O3JMD&keywords=swamp+thing+absolute&qid=1699815970&sprefix=swamp+thing+absolute%2Caps%2C106&sr=8-4&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47'
   },
   {
     nombre: 'Batman: Detective Comics 1000: The Deluxe Edition',
@@ -26,56 +31,43 @@ export const productos = [
     buyLink: 'https://www.amazon.com.mx/Detective-Comics-1000-Deluxe-Various/dp/1401294197/ref=sr_1_1?keywords=detective+comics+1000&qid=1692732668&sprefix=detective+comics%2Caps%2C114&sr=8-1&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47'
   },
   {
-    nombre: 'Flash omnibus',
-    img: 'https://m.media-amazon.com/images/I/81z6GJfR2PS._SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/1779513453/ref=ewc_pr_img_1?smid=A3KP3XLKQCJA4J&psc=1',
-    note: "Este esta a buen precio pero lo tienes que pedir con prime para que no cobren envio"
-  },
-  {
-    nombre: 'Gotham Central Omnibus',
-    img: 'https://m.media-amazon.com/images/I/91FWAFzkIlL.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/1779515634/ref=ox_sc_saved_image_2?smid=AVDBXBAVVSXLQ&psc=1',
-    note: "Este anda a buen precio"
+    nombre: 'Batman One Dark Knight',
+    img: 'https://m.media-amazon.com/images/I/81Jh-vB2TUL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Batman-One-Dark-Knight-Jock/dp/1779510284/ref=d_pd_sbs_sccl_2_2/137-9444642-6765259?pd_rd_w=bosYo&content-id=amzn1.sym.bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_p=bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_r=J9GPAV93F1SB1SVPAAH7&pd_rd_wg=wLpzz&pd_rd_r=2495d038-e053-4441-9fc4-07bd5bdcbed6&pd_rd_i=1779510284&psc=1',
+    note: "Este se ve muy bueno es igual un pasta dura gringo"
   },
   {
     nombre: 'Teenage Mutant Ninja Turtles: The Last Ronin ',
     img: 'https://m.media-amazon.com/images/I/81kQlzqKP-L.jpg',
     buyLink: 'https://www.amazon.com.mx/Teenage-Mutant-Ninja-Turtles-Ronin/dp/1684058414/ref=sr_1_1?__mk_es_MX=ÅMÅŽÕÑ&crid=2QGQFD61W4089&keywords=The+Last+Ronin&qid=1692425842&sprefix=the+last+ronin%2Caps%2C105&sr=8-1&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47',
   },
-  // {
-  //   nombre: 'JLA: The Tower of Babel',
-  //   img: 'https://m.media-amazon.com/images/I/91WQUDdSckL._SL1500_.jpg',
-  //   buyLink: 'https://www.amazon.com.mx/gp/product/1779509510?smid=AVDBXBAVVSXLQ&psc=1',
-  //   note: "Este no es omni, es un pasta dura gringo "
-  // },
-  // {
-  //   nombre: 'Batman One Dark Knight',
-  //   img: 'https://m.media-amazon.com/images/I/81Jh-vB2TUL._SL1500_.jpg',
-  //   buyLink: 'https://www.amazon.com.mx/Batman-One-Dark-Knight-Jock/dp/1779510284/ref=d_pd_sbs_sccl_2_2/137-9444642-6765259?pd_rd_w=bosYo&content-id=amzn1.sym.bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_p=bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_r=J9GPAV93F1SB1SVPAAH7&pd_rd_wg=wLpzz&pd_rd_r=2495d038-e053-4441-9fc4-07bd5bdcbed6&pd_rd_i=1779510284&psc=1',
-  //   note: "Este se ve muy bueno es igual un pasta dura gringo"
-  // },
   {
-    nombre: 'EL INMORTAL HULK VOL.06 (DE 12)',
-    img: 'https://m.media-amazon.com/images/I/71q+Eg2H9xL.jpg',
-    buyLink: 'https://www.amazon.com.mx/EL-INMORTAL-HULK-VOL-06-12/dp/6075781870/ref=sr_1_1?keywords=inmortal+hulk&qid=1692733233&sprefix=inmortal%2Caps%2C123&sr=8-1'
+    nombre: 'JLA: The Tower of Babel',
+    img: 'https://m.media-amazon.com/images/I/91WQUDdSckL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1779509510?smid=AVDBXBAVVSXLQ&psc=1',
+    note: "Este no es omni, es un pasta dura gringo "
   },
-  // {
-  //   nombre: 'Cosmic Oddisey',
-  //   img: 'https://http2.mlstatic.com/D_NQ_NP_2X_762398-MLM49917575671_052022-F.webp',
-  //   buyLink: 'https://www.mercadolibre.com.mx/dc-comic-oddisey/p/MLM21609649#polycard_client=bookmarks'
-  // },
+  {
+    nombre: 'McFarlane Megafigura de Accion DC Multiverse Clayface',
+    img: 'https://m.media-amazon.com/images/I/71+Q-EHk0dL._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/dp/B09DHGWLGK/?coliid=IBYME0W4XU3F8&colid=13QCSLA5P0HV2&psc=1&ref_=list_c_wl_lv_ov_lig_dp_it'
+  },
+  {
+    nombre: 'McFarlane Toys DC Multiverse Man-Bat, 9" - 1 Unidad',
+    img: 'https://m.media-amazon.com/images/I/81uF0Qf1NxL._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/B09SYS41SK/ref=ox_sc_saved_image_14?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  {
+    nombre: 'Scarecrow mcfarlane',
+    img: 'https://m.media-amazon.com/images/I/81RZucRTUoL._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/McFarlane-Gaming-Figures-Wave-Scarecrow/dp/B09V6PJYGC/ref=sr_1_6?__mk_es_MX=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3OB4QPGFUDX07&keywords=batman+scarecrow+mcfarlane&qid=1695687055&sprefix=batman+scarecrow+mcfarlane%2Caps%2C104&sr=8-6&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47'
+  },
   {
     nombre: 'McFarlane Toys DC Multiverse The Dark Knight Return Batman',
     img: 'https://m.media-amazon.com/images/I/61nhlBozr2L._AC_SL1500_.jpg',
     buyLink: 'https://www.amazon.com.mx/gp/product/B09BF32X81/ref=ox_sc_saved_image_3?smid=AVDBXBAVVSXLQ&psc=1',
     note: ""
   },
-  // {
-  //   nombre: 'The Legend of Zelda: Skyward Sword HD ',
-  //   img: 'https://m.media-amazon.com/images/I/81ty2Tz3OfS._AC_SL1500_.jpg',
-  //   buyLink: 'https://www.amazon.com.mx/dp/B092YJ62Y6/?coliid=I3U0UXT5BVEDNG&colid=3CW9Y6HRWAB2Z&psc=1&ref_=lv_ov_lig_dp_it',
-  //   note: 'Todos los juegos de switch cambian a diario de precio no conviene comprarlos si cuestan mas de 1100'
-  // },
   {
     nombre: 'Estuche Rígido Negro para Nintendo Switch',
     img: 'https://resources.sanborns.com.mx/imagenes-sanborns-ii/1200/663293109128.jpg',
@@ -83,19 +75,9 @@ export const productos = [
     note: 'Este es el que vimos en sanborns pero si encuentras otro igual esta bien'
   },
   {
-    nombre: 'ESTUCHE RIGIDO NINTENDO SWITCH OLED',
-    img: 'https://cdn.gameplanet.com/wp-content/uploads/2023/02/07195651/810050910989-estuche-hori-rigido-oled-1.jpg',
-    buyLink: 'https://gameplanet.com/producto/estuche-rigido-nintendo-switch-oled-nsw/'
-  },
-  {
-    nombre: 'CHAINSAW MAN N.12',
-    img: 'https://d14d9vp3wdof84.cloudfront.net/image/589816272436/image_871qje5sph31pemon5s3cfu75a/-S897-FWEBP',
-    note: "Este alomejor todavia lo encuentras en sanborns"
-  },
-  {
     nombre: 'STAR WARS La colección Vintage - Juguete ARC Trooper',
-    img: 'https://m.media-amazon.com/images/I/81txVzCPA-L._AC_SL1500_.jpg',
-    buyLink: 'https://articulo.mercadolibre.com.mx/MLM-1397340182-figura-de-accion-star-wars-the-vintage-collection-4-_JM#position=11&search_layout=stack&type=item&tracking_id=b2ec254f-b112-481f-bbe3-6c6c3244a149',
+    img: 'https://http2.mlstatic.com/D_NQ_NP_987354-MLA49463350465_032022-O.webp',
+    buyLink: 'https://www.mercadolibre.com.mx/figura-accion-hasbro-star-wars-arc-trooper-captain/p/MLM19731155#reco_item_pos=0&reco_backend=recomm-platform_v2p_univb&reco_backend_type=low_level&reco_client=vip-v2p&reco_id=ce2c96ef-9fc4-4b15-b4bf-c1dcde275802',
     note: ""
   },
   {
