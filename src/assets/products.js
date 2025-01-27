00ff13adeb550f72092f71c7305510b15e30cec9
Alucard <xavier_ortega98@hotmail.com>
2025-01-27 16:02:32 -0600

    PM 27/01

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,43 +1,66 @@
 export const productos = [
-  {
-    nombre: 'Astro Bot',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_906762-MLU78441428060_082024-F.webp',
-    buyLink: 'https://www.mercadolibre.com.mx/astro-bot/p/MLM39408566#wid%3DMLM2127171897%26sid%3Dsearch%26searchVariation%3DMLM39408566%26position%3D3%26search_layout%3Dstack%26type%3Dproduct%26tracking_id%3D0e984cfe-5eae-4dce-b13c-0d072069370e&gid=1&pid=1',
-  },
-  {
-    nombre: 'Dead Space',
-    img: 'https://m.media-amazon.com/images/I/71ewiYjV0GL._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/dp/B0BGVTBWJP/?coliid=I2GUA39H24ANLZ&colid=3CYU13C5PJHUB&ref_=list_c_wl_lv_ov_lig_dp_it_im&th=1',
-  },
-  {
-    nombre: 'Cubiertas de la consola PlayStation®5 SLIM: Midnight Black',
-    img: 'https://m.media-amazon.com/images/I/51FUnzIiopL._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/dp/B0CSF244P6/?coliid=IOTV6LT8SREKV&colid=3CYU13C5PJHUB&psc=1&ref_=list_c_wl_lv_ov_lig_dp_it',
-  },
-  {
-    nombre: 'PlayStation DualSense® Wireless Controller - Midnight Black - Midnight Black Edition',
-    img: 'https://m.media-amazon.com/images/I/61WRPyQAIIL._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/dp/B0CQKKHT5J/?coliid=IGBR4ZAWBXSBL&colid=3CYU13C5PJHUB&ref_=list_c_wl_lv_ov_lig_dp_it&th=1',
-  },
-  {
-    nombre: 'MEGA Halo Juguete de Construcción Tiroteo en la Compuerta',
-    img: 'https://m.media-amazon.com/images/I/81g+AclH3OL._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/dp/B0BBSQQNF9/?coliid=I3N0R9M3VF72ZF&colid=13QCSLA5P0HV2&ref_=list_c_wl_lv_ov_lig_dp_it&th=1',
-    note: "igual esta en mercadolibre"
-  },
+  // {
+  //   nombre: 'Astro Bot',
+  //   img: 'https://http2.mlstatic.com/D_NQ_NP_2X_906762-MLU78441428060_082024-F.webp',
+  //   buyLink: 'https://www.mercadolibre.com.mx/astro-bot/p/MLM39408566#wid%3DMLM2127171897%26sid%3Dsearch%26searchVariation%3DMLM39408566%26position%3D3%26search_layout%3Dstack%26type%3Dproduct%26tracking_id%3D0e984cfe-5eae-4dce-b13c-0d072069370e&gid=1&pid=1',
+  // },
+  // {
+    //   nombre: 'Dead Space',
+    //   img: 'https://m.media-amazon.com/images/I/71ewiYjV0GL._AC_SL1500_.jpg',
+    //   buyLink: 'https://www.amazon.com.mx/dp/B0BGVTBWJP/?coliid=I2GUA39H24ANLZ&colid=3CYU13C5PJHUB&ref_=list_c_wl_lv_ov_lig_dp_it_im&th=1',
+    // },
+    // {
+      //   nombre: 'Cubiertas de la consola PlayStation®5 SLIM: Midnight Black',
+      //   img: 'https://m.media-amazon.com/images/I/51FUnzIiopL._AC_SL1500_.jpg',
+      //   buyLink: 'https://www.amazon.com.mx/dp/B0CSF244P6/?coliid=IOTV6LT8SREKV&colid=3CYU13C5PJHUB&psc=1&ref_=list_c_wl_lv_ov_lig_dp_it',
+      // },
+      // {
+  //   nombre: 'PlayStation DualSense® Wireless Controller - Midnight Black - Midnight Black Edition',
+  //   img: 'https://m.media-amazon.com/images/I/61WRPyQAIIL._AC_SL1500_.jpg',
+  //   buyLink: 'https://www.amazon.com.mx/dp/B0CQKKHT5J/?coliid=IGBR4ZAWBXSBL&colid=3CYU13C5PJHUB&ref_=list_c_wl_lv_ov_lig_dp_it&th=1',
+  // },
+  // {
+    //   nombre: 'MEGA Halo Juguete de Construcción Tiroteo en la Compuerta',
+  //   img: 'https://m.media-amazon.com/images/I/81g+AclH3OL._AC_SL1500_.jpg',
+  //   buyLink: 'https://www.amazon.com.mx/dp/B0BBSQQNF9/?coliid=I3N0R9M3VF72ZF&colid=13QCSLA5P0HV2&ref_=list_c_wl_lv_ov_lig_dp_it&th=1',
+  //   note: "igual esta en mercadolibre"
+  // },
   {
     nombre: 'Supergirl: Woman of Tomorrow',
     img: 'https://m.media-amazon.com/images/I/81tZMT5JkSL._SL1500_.jpg',
     buyLink: 'https://www.amazon.com.mx/dp/1779526075/?coliid=I2UIUM1YY931CS&colid=38XGHRYT7U715&psc=0&ref_=list_c_wl_lv_ov_lig_dp_it',
-    note: "Este lo he esperado mucho pero sale hasta el 23 de julio"
+    note: ""
   },
   {
-    nombre: 'Batman: Detective Comics 1: Gotham Nocturne: Overture',
-    img: 'https://m.media-amazon.com/images/I/813c+THnPKL._SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/Batman-Detective-Comics-Ram-V/dp/1779520948/ref=sr_1_53?__mk_es_MX=ÅMÅŽÕÑ&crid=D5VCX4MXK3K8&keywords=dc+hardcover&qid=1707540824&sprefix=dc+hardcover%2Caps%2C128&sr=8-53&ufe=app_do%3Aamzn1.fos.4e545b5e-1d45-498b-8193-a253464ffa47',
+    nombre: 'Batman the Cult',
+    img: 'https://m.media-amazon.com/images/I/917m1NweXGL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/1779528272/ref=ox_sc_act_image_1?smid=A1G99GVHAT2WD8&psc=1',
+    note: ""
+  },
+  {
+    nombre: 'Batman: Gotham Nocturne Act 1',
+    img: 'https://m.media-amazon.com/images/I/71qZyQPGfWL._SL1200_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Batman-Gotham-Nocturne-Act-1/dp/6075827765/ref=sr_1_3?crid=3V61WZCZA946T&dib=eyJ2IjoiMSJ9.wQQVaMrT3GOYEionNvvz_7QSSsbQFa_dK0jcO_qJEXx-mzsW_ynZI2rCvwRdy6kuAzv2SxQ1ezH7rXAUiwpL9a6w1j1KOK965SEg-LoXXIv8ZzJkcjk4yOh2jssmrFjeGuWQKabphHeD90XqIRmNyM-vY41kR8Dl2k8GiW1_vmnz-4DbuPUkEKp6GoudhHVh0qEV8jS0YhkrdhDSf1F3kgNle-F_3kN08A4jP-PUoFWHc6chIDE4hm4PU-0F-lUR6gGyt4tDywbSymO7Jmt5_0iXsbpWCK7gVkJEHLfUfY7MLTxeUkczlTEBppu3t_5qpyuurmIPMlpy1yPMtQCy6aVlfsL33du0NR57bVdU02lvH8AaUAOb0-MaoABs6MjuQWUNR-IabcqsMYz7zCYC3aVGme3kqGVrlM5DvSfjdIzWr7YUMhapgA1-60kBMKHG.uKrfICe42g3LPxyCpByy6hyi-FI8SznC01wNirK54cM&dib_tag=se&keywords=batman+detective+comics&qid=1738015176&sprefix=batman+de%2Caps%2C145&sr=8-3',
+    note: ""
+  },
+  {
+    nombre: 'Bleach Remix N.2',
+    img: 'https://m.media-amazon.com/images/I/71y1eLugjWL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/607582443X/ref=ox_sc_saved_image_3?smid=AVDBXBAVVSXLQ&psc=1',
+    note: ""
+  },
+  {
+    nombre: 'McFarlane Toys DC Multiverse Man-Bat, 9" - 1 Unidad',
+    img: 'https://m.media-amazon.com/images/I/81uF0Qf1NxL._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/B09SYS41SK/ref=ox_sc_saved_image_14?smid=AVDBXBAVVSXLQ&psc=1'
+  },
+  {
+    nombre: 'Super Mario Odyssey ',
+    img: 'https://m.media-amazon.com/images/I/91SF0Tzmv4L._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Super-Mario-Odyssey-Nintendo-Standard/dp/B01MY7GHKJ/ref=sr_1_1?dib=eyJ2IjoiMSJ9.CcYjReheVRt_WL8IhWwWkxHV43ff5KuK50ohw9sILDwH8yAa71xIY-TMlQ3qHMyZQAr6vnqcZA7F4XRd29UYm0WIzhRzq2BEqSpGoCmlYlJIuPVITHe2JcWlU9Nm0b_b8-g8pbpxFbmOgwNG186r0adBCc4_jpX6sgJXGvdOYK_1ebbppZ4_PFgkEhJnVVV_GnK4eIiOhcIMEwV8HXUI_GYX-X2Hmzv_2qq9Toe4Z5sPOv0UL9sos9nW1WWI9h1JN8jDONhSF-Bl5ZMvS_4cWIkbTd5I8eaT49ICe4tLA09CbCztXCjGg2IHEe6IOMf-0JCiF3PjVM079AxI3IZfVLsy0J0RdPLLKjZ60fPASXoCTWoqoQ4yc3bbZZG4kqxViocKyUy23jSMu_QxEpPlyu87jkpJ_qM8G4k8HHkP6fwel7m_ObHDIUNtJ87M-0YB.t9CSfjqf-sGyDnZU9tya7LXTNnALLVWMjtMIIKRpWas&dib_tag=se&keywords=mario+odyssey&qid=1736293604&sr=8-1&ufe=app_do%3Aamzn1.fos.de93fa6a-174c-4df7-be7c-5bc8e9c5a71b',
   },
   // {
-  //   nombre: 'Batman One Dark Knight',
+    //   nombre: 'Batman One Dark Knight',
   //   img: 'https://m.media-amazon.com/images/I/81Jh-vB2TUL._SL1500_.jpg',
   //   buyLink: 'https://www.amazon.com.mx/Batman-One-Dark-Knight-Jock/dp/1779510284/ref=d_pd_sbs_sccl_2_2/137-9444642-6765259?pd_rd_w=bosYo&content-id=amzn1.sym.bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_p=bf51709a-d6bb-4474-b305-30d2f8a81b78&pf_rd_r=J9GPAV93F1SB1SVPAAH7&pd_rd_wg=wLpzz&pd_rd_r=2495d038-e053-4441-9fc4-07bd5bdcbed6&pd_rd_i=1779510284&psc=1',
   //   note: "Este se ve muy bueno es igual un pasta dura gringo"
@@ -47,11 +70,6 @@ export const productos = [
   //   img: 'https://m.media-amazon.com/images/I/91OFMZdGs7L._SL1500_.jpg',
   //   buyLink: 'https://www.amazon.com.mx/Justice-Deluxe-Jim-Krueger/dp/1401293433/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=1700507156&sr=8-66'
   // },
-  {
-    nombre: 'McFarlane Toys DC Multiverse Man-Bat, 9" - 1 Unidad',
-    img: 'https://m.media-amazon.com/images/I/81uF0Qf1NxL._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/B09SYS41SK/ref=ox_sc_saved_image_14?smid=AVDBXBAVVSXLQ&psc=1'
-  },
   // {
   //   nombre: 'McFarlane Toys DC Multiverse The Dark Knight Return Batman',
   //   img: 'https://m.media-amazon.com/images/I/61nhlBozr2L._AC_SL1500_.jpg',
