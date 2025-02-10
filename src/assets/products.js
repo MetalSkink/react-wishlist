412995deb94506e70d5a0cd91fd10ba347f52b71
Alucard <xavier_ortega98@hotmail.com>
2025-02-10 11:49:01 -0600

    PM 10/02

--- src/assets/products.js
+++ src/assets/products.js
@@ -25,6 +25,34 @@ export const productos = [
   //   buyLink: 'https://www.amazon.com.mx/dp/B0BBSQQNF9/?coliid=I3N0R9M3VF72ZF&colid=13QCSLA5P0HV2&ref_=list_c_wl_lv_ov_lig_dp_it&th=1',
   //   note: "igual esta en mercadolibre"
   // },
+  // {
+  //   nombre: 'SAMSUNG Audífonos Inalámbricos Galaxy Buds FE Bluetooth Cancelación de Ruido Estuche Color Blanco',
+  //   img: 'https://m.media-amazon.com/images/I/51oK-4LQw6L._AC_SL1500_.jpg',
+  //   note: "Estos estan bonitos cuestan como 1400, me servirian un buen"
+  // },
+  {
+    nombre: 'Marvel Legends Series, Jean Grey y Fuerza Fénix',
+    img: 'https://m.media-amazon.com/images/I/81973xXck8L._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Marvel-Legends-Fuerza-Coleccionable-Inspirada/dp/B0CSH14G2Y/ref=sr_1_75?__mk_es_MX=ÅMÅŽÕÑ&crid=2J5IOXUOB90OA&dib=eyJ2IjoiMSJ9.tvdyEb9RFjSjuGS8XZSIeHHu3KiaoAldx-c3xv2TMRHdazg9kkeQekVu7Ayu4k4ULh2SOPaEaHFYoMGREmHzBwuKe-dwhdgiyczc2PI127OoMCsFbgqllK15RVjcISQNthonqqGpn9OwdRhgoGRwtIXchXb8dU-H5KCeZOTvPvd2DfgRrC--3BBKJGJhGcIOnArj2gmsZ9RB6ielJvHlazto1WYXrkaD_BQzKwxEYWTlHZybc3MFDff8Mr2lcFMbygQAGUZw5_pupASC10OLaLbN19f3plTQJ5WveanX5OHosWI0aGOBHe2SSjUoW7Jc8h6RzrVPAOctUxPe0JikCsQa2vXL9jkOTj7Ja9ClISPDngO7eS9PWGMBcdCXIxi_c77vHldU5po4VDgUm0LNAXSvTypen5RMUdacEp_TPRJC06Dc--YJs7QNT-Nkt1bx.qGbMcMbe7Bbac3FLx7itiFuTJZf_oGn09PFCVtKA5Ec&dib_tag=se&keywords=Marvel+Legends&qid=1739206897&sprefix=marvel+legends%2Caps%2C123&sr=8-75&ufe=app_do%3Aamzn1.fos.de93fa6a-174c-4df7-be7c-5bc8e9c5a71b&xpid=ucY1nSTopaSw7',
+    note: ""
+  },
+  {
+    nombre: 'Batman: Gotham Nocturne Act 1',
+    img: 'https://m.media-amazon.com/images/I/71qZyQPGfWL._SL1200_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Batman-Gotham-Nocturne-Act-1/dp/6075827765/ref=sr_1_3?crid=3V61WZCZA946T&dib=eyJ2IjoiMSJ9.wQQVaMrT3GOYEionNvvz_7QSSsbQFa_dK0jcO_qJEXx-mzsW_ynZI2rCvwRdy6kuAzv2SxQ1ezH7rXAUiwpL9a6w1j1KOK965SEg-LoXXIv8ZzJkcjk4yOh2jssmrFjeGuWQKabphHeD90XqIRmNyM-vY41kR8Dl2k8GiW1_vmnz-4DbuPUkEKp6GoudhHVh0qEV8jS0YhkrdhDSf1F3kgNle-F_3kN08A4jP-PUoFWHc6chIDE4hm4PU-0F-lUR6gGyt4tDywbSymO7Jmt5_0iXsbpWCK7gVkJEHLfUfY7MLTxeUkczlTEBppu3t_5qpyuurmIPMlpy1yPMtQCy6aVlfsL33du0NR57bVdU02lvH8AaUAOb0-MaoABs6MjuQWUNR-IabcqsMYz7zCYC3aVGme3kqGVrlM5DvSfjdIzWr7YUMhapgA1-60kBMKHG.uKrfICe42g3LPxyCpByy6hyi-FI8SznC01wNirK54cM&dib_tag=se&keywords=batman+detective+comics&qid=1738015176&sprefix=batman+de%2Caps%2C145&sr=8-3',
+    note: "Este lo puedes encontrar en sanborns"
+  },
+  {
+    nombre: 'Bleach Remix N.2',
+    img: 'https://m.media-amazon.com/images/I/71y1eLugjWL._SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/607582443X/ref=ox_sc_saved_image_3?smid=AVDBXBAVVSXLQ&psc=1',
+    note: "De este me faltan del 2 al 5"
+  },
+  {
+    nombre: 'McFarlane Toys DC Multiverse Man-Bat, 9" - 1 Unidad',
+    img: 'https://m.media-amazon.com/images/I/81uF0Qf1NxL._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/gp/product/B09SYS41SK/ref=ox_sc_saved_image_14?smid=AVDBXBAVVSXLQ&psc=1'
+  },
   {
     nombre: 'Supergirl: Woman of Tomorrow',
     img: 'https://m.media-amazon.com/images/I/81tZMT5JkSL._SL1500_.jpg',
@@ -37,28 +65,11 @@ export const productos = [
     buyLink: 'https://www.amazon.com.mx/gp/product/1779528272/ref=ox_sc_act_image_1?smid=A1G99GVHAT2WD8&psc=1',
     note: ""
   },
-  {
-    nombre: 'Batman: Gotham Nocturne Act 1',
-    img: 'https://m.media-amazon.com/images/I/71qZyQPGfWL._SL1200_.jpg',
-    buyLink: 'https://www.amazon.com.mx/Batman-Gotham-Nocturne-Act-1/dp/6075827765/ref=sr_1_3?crid=3V61WZCZA946T&dib=eyJ2IjoiMSJ9.wQQVaMrT3GOYEionNvvz_7QSSsbQFa_dK0jcO_qJEXx-mzsW_ynZI2rCvwRdy6kuAzv2SxQ1ezH7rXAUiwpL9a6w1j1KOK965SEg-LoXXIv8ZzJkcjk4yOh2jssmrFjeGuWQKabphHeD90XqIRmNyM-vY41kR8Dl2k8GiW1_vmnz-4DbuPUkEKp6GoudhHVh0qEV8jS0YhkrdhDSf1F3kgNle-F_3kN08A4jP-PUoFWHc6chIDE4hm4PU-0F-lUR6gGyt4tDywbSymO7Jmt5_0iXsbpWCK7gVkJEHLfUfY7MLTxeUkczlTEBppu3t_5qpyuurmIPMlpy1yPMtQCy6aVlfsL33du0NR57bVdU02lvH8AaUAOb0-MaoABs6MjuQWUNR-IabcqsMYz7zCYC3aVGme3kqGVrlM5DvSfjdIzWr7YUMhapgA1-60kBMKHG.uKrfICe42g3LPxyCpByy6hyi-FI8SznC01wNirK54cM&dib_tag=se&keywords=batman+detective+comics&qid=1738015176&sprefix=batman+de%2Caps%2C145&sr=8-3',
-    note: ""
-  },
-  {
-    nombre: 'Bleach Remix N.2',
-    img: 'https://m.media-amazon.com/images/I/71y1eLugjWL._SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/607582443X/ref=ox_sc_saved_image_3?smid=AVDBXBAVVSXLQ&psc=1',
-    note: ""
-  },
-  {
-    nombre: 'McFarlane Toys DC Multiverse Man-Bat, 9" - 1 Unidad',
-    img: 'https://m.media-amazon.com/images/I/81uF0Qf1NxL._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/gp/product/B09SYS41SK/ref=ox_sc_saved_image_14?smid=AVDBXBAVVSXLQ&psc=1'
-  },
-  {
-    nombre: 'Super Mario Odyssey ',
-    img: 'https://m.media-amazon.com/images/I/91SF0Tzmv4L._AC_SL1500_.jpg',
-    buyLink: 'https://www.amazon.com.mx/Super-Mario-Odyssey-Nintendo-Standard/dp/B01MY7GHKJ/ref=sr_1_1?dib=eyJ2IjoiMSJ9.CcYjReheVRt_WL8IhWwWkxHV43ff5KuK50ohw9sILDwH8yAa71xIY-TMlQ3qHMyZQAr6vnqcZA7F4XRd29UYm0WIzhRzq2BEqSpGoCmlYlJIuPVITHe2JcWlU9Nm0b_b8-g8pbpxFbmOgwNG186r0adBCc4_jpX6sgJXGvdOYK_1ebbppZ4_PFgkEhJnVVV_GnK4eIiOhcIMEwV8HXUI_GYX-X2Hmzv_2qq9Toe4Z5sPOv0UL9sos9nW1WWI9h1JN8jDONhSF-Bl5ZMvS_4cWIkbTd5I8eaT49ICe4tLA09CbCztXCjGg2IHEe6IOMf-0JCiF3PjVM079AxI3IZfVLsy0J0RdPLLKjZ60fPASXoCTWoqoQ4yc3bbZZG4kqxViocKyUy23jSMu_QxEpPlyu87jkpJ_qM8G4k8HHkP6fwel7m_ObHDIUNtJ87M-0YB.t9CSfjqf-sGyDnZU9tya7LXTNnALLVWMjtMIIKRpWas&dib_tag=se&keywords=mario+odyssey&qid=1736293604&sr=8-1&ufe=app_do%3Aamzn1.fos.de93fa6a-174c-4df7-be7c-5bc8e9c5a71b',
-  },
+  // {
+  //   nombre: 'Super Mario Odyssey ',
+  //   img: 'https://m.media-amazon.com/images/I/91SF0Tzmv4L._AC_SL1500_.jpg',
+  //   buyLink: 'https://www.amazon.com.mx/Super-Mario-Odyssey-Nintendo-Standard/dp/B01MY7GHKJ/ref=sr_1_1?dib=eyJ2IjoiMSJ9.CcYjReheVRt_WL8IhWwWkxHV43ff5KuK50ohw9sILDwH8yAa71xIY-TMlQ3qHMyZQAr6vnqcZA7F4XRd29UYm0WIzhRzq2BEqSpGoCmlYlJIuPVITHe2JcWlU9Nm0b_b8-g8pbpxFbmOgwNG186r0adBCc4_jpX6sgJXGvdOYK_1ebbppZ4_PFgkEhJnVVV_GnK4eIiOhcIMEwV8HXUI_GYX-X2Hmzv_2qq9Toe4Z5sPOv0UL9sos9nW1WWI9h1JN8jDONhSF-Bl5ZMvS_4cWIkbTd5I8eaT49ICe4tLA09CbCztXCjGg2IHEe6IOMf-0JCiF3PjVM079AxI3IZfVLsy0J0RdPLLKjZ60fPASXoCTWoqoQ4yc3bbZZG4kqxViocKyUy23jSMu_QxEpPlyu87jkpJ_qM8G4k8HHkP6fwel7m_ObHDIUNtJ87M-0YB.t9CSfjqf-sGyDnZU9tya7LXTNnALLVWMjtMIIKRpWas&dib_tag=se&keywords=mario+odyssey&qid=1736293604&sr=8-1&ufe=app_do%3Aamzn1.fos.de93fa6a-174c-4df7-be7c-5bc8e9c5a71b',
+  // },
   // {
     //   nombre: 'Batman One Dark Knight',
   //   img: 'https://m.media-amazon.com/images/I/81Jh-vB2TUL._SL1500_.jpg',
