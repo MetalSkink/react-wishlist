9ec18ea177388a45cce5e09ece46c534bb59324a
Alucard <xavier_ortega98@hotmail.com>
2025-12-11 11:27:54 -0600

    pu 1112

--- src/assets/products.js
+++ src/assets/products.js
@@ -18,6 +18,12 @@ export const productos = [
     note: ""
   },
   {
+    nombre: 'MARVEL Legends Series, Máscara Premium de Wolverine',
+    img: 'https://m.media-amazon.com/images/I/71NTWbONDuL._AC_SL1500_.jpg',
+    buyLink: 'https://www.amazon.com.mx/Marvel-MVL-Legends-Gear-2/dp/B0DK7NZMGM/ref=sr_1_5?crid=38Y03CTH1T60K&dib=eyJ2IjoiMSJ9._MFvnAtPfpH_foIePuRk1KmKm4YgFtctgQPfPF0Vn3gQWS31krOLKF0XYP_ZewjzDDE7vPLRCF0rid_OHh2RMxTJC2UZNuEPCy6OPaMxcRezULrtc-T7AQyZrOzX3PIhbzhIvVRuM0vi14yrJVc1pJ9oGEnTFJw3_32SrOewIFP_4pxLYywmsgoKGoUpo4NT9SRgTYw-TLEpmgMFjIc2PEpENnDNh5NgWj9defWh1e3hd1OnAKf6z3mcvo_HUgBFZHnMljCPNVdJzmwYDG2IjvV-7m1eoyksO6ObVs9l4cc.kSimEf2m0b0xxUgV6oEAKQPHI9vtpTGSKrAU2S02z_o&dib_tag=se&keywords=casco+wolverine&qid=1765473842&sprefix=casco+wol%2Caps%2C128&sr=8-5&ufe=app_do%3Aamzn1.fos.e9c905c0-296e-4852-9ea7-c9f4c88300d3',
+    note: ""
+  },
+  {
     nombre: 'Wonder Woman by Greg Rucka Omnibus',
     img: 'https://m.media-amazon.com/images/I/81O84-mLN9L._SL1500_.jpg',
     buyLink: 'https://www.amazon.com.mx/Wonder-Woman-Greg-Rucka-Omnibus/dp/1799501574/ref=sr_1_2?crid=230AMKQIAW1CE&dib=eyJ2IjoiMSJ9.T66jIMGgYKZnKk836j-ZF5JD9AVPE-Fg62Walh6g6cFYK1gTmGTk98V0QKFIcX0Vv173FxhAqaTC2J8yoPfp7jG7MWJyXtrQl00ugZ3MdSOtwKusLafZQSLt4Gzm20Qy3oi5E07wfFj18lBFN5NhJG-z1ZugLlZ3PtoO5qp7fM5asnBN7TYehkOw-h2LNOi_aaGK7F6mtCZDqSh3ehGqpmF7no4MKfHYNTt_nz9Z626Sv4ur4zGQ7Byet_T1sByjOw0YCUyrGLdR2ZZtmsYh7V-G1jtQJVGhkAlYlnq4uHU.0z6qPMyC0dkQ2PAicEgXf25N-k6HUCfwnZtU3JCK_z0&dib_tag=se&keywords=wonder+woman+omnibus&qid=1762757144&sprefix=wonder+woman+om%2Caps%2C115&sr=8-2&ufe=app_do%3Aamzn1.fos.3ad1a8a7-7417-4467-b505-11834e377abf',
@@ -82,12 +88,6 @@ export const productos = [
     img: 'https://m.media-amazon.com/images/I/91cgm06eMlL._SL1500_.jpg',
     buyLink: 'https://www.amazon.com.mx/dp/1799502694/?coliid=I3ECV9APFKNWXB&colid=38XGHRYT7U715&psc=0&ref_=list_c_wl_lv_ov_lig_dp_it',
     note: "Este sale el 2 de diciembre perfecto para navidad"
-  },
-  {
-    nombre: 'Hot Wheels Premium Red Bull Racing RB20 F1',
-    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_990963-MLA95675087402_102025-F.webp',
-    buyLink: 'https://www.mercadolibre.com.mx/hot-wheels-premium-red-bull-racing-rb20-f1/p/MLM53751545#polycard_client=recommendations_home_affiliate-profile&reco_backend=item_decorator&reco_client=home_affiliate-profile&reco_item_pos=0&source=affiliate-profile&reco_backend_type=function&reco_id=5c674c37-35be-4c08-812a-a0640f1d5beb&tracking_id=753b8d33-12f0-41e1-8999-9562537a98ac&wid=MLM4254444892&sid=recos&c_id=/home/card-featured/element&c_uid=3ec9bea0-2548-4f37-95b0-382f428c9b2a',
-    note: ""
   },
   {
     nombre: 'Green Arrow: Archers Quest Omnibus Vol. 1  ',
